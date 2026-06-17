5996f439d8162e765d11227d2335ea83076c730c
Vicky-Developer28 <vicky28.developer@gmail.com>
2026-06-17 17:25:58 +0530

    delete all urls

--- zumex/urls.py
+++ zumex/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path('privacy-policy/', TemplateView.as_view(template_name='zumex/privacy_policy.html'), name='privacy_policy'),
     path('terms-of-service/', TemplateView.as_view(template_name='zumex/terms_of_service.html'), name='terms_of_service'),
 ]
+ 
